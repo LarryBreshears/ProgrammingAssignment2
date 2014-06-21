22ad083c836f2e20c50b95bbfe52f27e595cf918
LarryBreshears <larrybreshears@hotmail.com>
2014-06-21 16:37:54 -0400

    comment update

--- cachematrix.R
+++ cachematrix.R
@@ -48,10 +48,12 @@ cacheSolve <- function(x, ...) {
 
 #To use this combination of functions in the console:
 # 1. Source the above code
-# 2. use the set function to imput the matrix
-#    Example "m$set(matrix(c(0,2,2,0),2,2))
-# 3. Call the cacheSolve(m) function
+# 2. create a cashable matrix object
+#    Example:  m<- makeCacheMatrix( )
+# 3. use the set function to imput the matrix
+#    Example: m$set(matrix(c(0,2,2,0),2,2))
+# 4. Call the cacheSolve(m) function
 #    That will cach and solve the matrix if it is not
 #    already cached.
-# 4. call the cacheSolve(m) function a second time.
+# 5. call the cacheSolve(m) function a second time.
 #    That will return the cached/solved matrix.
